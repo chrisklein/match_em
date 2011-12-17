2151bf515513a5c35caff77bf4091a67fcb237e0
Christopher Klein <chrisklein303@gmail.com>
2011-12-17 10:37:01 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlipAndMatch::Application.config.secret_token = '8f766724fc6f33a0914c964c7ba2138400276bf1bcae22f1590ba14970badeffe44870eac1db160d9a7839c3f4c5355fcf6abbe6d25bc8497a2a0f25b520c3e5'
