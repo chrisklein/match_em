cb5093710df37be218d823ebe4b470a43454c199
Christopher Klein <chrisklein303@gmail.com>
2011-12-18 10:18:05 -0700

    stubbed out JS

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,4 +1,45 @@
-$(document).ready(function(){
-	alert('why twice/')
-})
+(function(){
+	var FipGame, FlipGameController
 	
+	FlipGame = {
+		init: function(){
+			this.Controller = new FlipGameController;
+			Backbone.history.start()
+		}
+	}
+	
+	this.FlipGame = FlipGame
+	
+	/**
+	 * Game Models
+	 */
+	Card = Backbone.Model.extend({ });
+
+	Deck = Backbone.Collection.extend({
+		model: Card
+	})
+	
+	
+	/**
+	 * Game Views
+	 */
+	
+	
+	/**
+	 * Game Controller
+	 */
+	FlipGameController = Backbone.Router.extend({
+		initialize: function( options ){
+			// Code if we need it.
+			},
+			
+		routes: {
+			""   : "root"
+			},	
+			
+		root: function(){
+			console.log('hello world')
+			}
+	});	
+	
+}).call(this);	
