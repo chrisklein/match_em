b464b8ce05a63f765c6c7871efa7709f56cb4fa8
Christopher Klein <chrisklein303@gmail.com>
2011-12-18 17:21:24 -0700

    first cut

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -5,20 +5,20 @@
 		init: function(){
 			this.Controller = new FlipGameController;
 			Backbone.history.start()
-		}
+		},
+		
+		flipped: 0
 	}
 	
 	this.FlipGame = FlipGame
 	
+	
 	/**
 	 * Game Models
 	 */
-	Card = Backbone.Model.extend({ 
-	});
+	Card = Backbone.Model.extend({ });
 
-	Deck = Backbone.Collection.extend({
-		model: Card
-	})
+	Deck = Backbone.Collection.extend({	model: Card })
 	
 	
 	/**
@@ -28,21 +28,34 @@
 		tagName: 'div',
 		
 		initialize: function(options){
-		},
+			_.bindAll(this, 'render');
+			this.model.bind('change', this.render);
+			},
 		
 		events: {
-		 'click span': 'click'	
-			
-		},
+		 	'click': 'click'	
+			},
 		
 		render: function(){
-			$(this.el).append( '<span>'+this.model.get('name')+'</span>' )
+			$(this.el).empty()
+			if( _.isEmpty(this.model.attributes) ){ this.remove() }
+			else{ this.model.get('flipped') ? this.face_up() : this.face_down() }
 			return this
-		},
+			},
+		
+		face_down: function(){
+			$(this.el).append( '<span class="card-back"></span>' )
+			},
+			
+		face_up: function(){
+			$(this.el).append( '<span class="'+this.model.get('name')+'"></span>' )
+			},	
 		
 		click: function(){
-			alert(this.model.get('name'))
-		}
+			if( !this.model.flipped ){ this.model.set({'flipped': true}); ++FlipGame.flipped }
+			this.render()
+			FlipGame.Controller.check_count()
+			}
 	})	
 	
 	
@@ -60,20 +73,35 @@
 			
 		root: function(){
 			Deck = new Deck([
-				{name: 'Moe',   flipped: false},
-				{name: 'Larry', flipped: false},
-				{name: 'Larry', flipped: false},
-				{name: 'Curly', flipped: false},
-				{name: 'Moe',   flipped: false},				
-				{name: 'Curly', flipped: false}
+				{name: 'moe',   flipped: false},
+				{name: 'larry', flipped: false},
+				{name: 'larry', flipped: false},
+				{name: 'curly', flipped: false},
+				{name: 'moe',   flipped: false},				
+				{name: 'curly', flipped: false}
 				])
-
 			
 			$("td").each(function (i) {  
-				var card_view = new CardView({ model: Deck.at(i) })     
-				$(this).append(  card_view.render().el    )       	
+				var card = Deck.at(i)
+				var card_view = new CardView({ model: card })     
+				$(this).append( card_view.render().el )       	
 				})
 			
+			},
+			
+		check_count: function(){
+			if(FlipGame.flipped %2 == 0){ var t=setTimeout("FlipGame.Controller.check_match()",1000); }
+			},
+			
+		check_match: function(){
+			var flipped_cards = Deck.filter(function(card){ return card.get('flipped') == true })
+			if(flipped_cards[0].get('name') == flipped_cards[1].get('name')){_(flipped_cards).each(function(card){ card.clear() }) }
+			else{ _(flipped_cards).each(function(card){ card.set({ 'flipped': false }); FlipGame.flipped = FlipGame.flipped - 1 }) }
+			if(Deck.all(function(card){ return _.isEmpty(card.attributes) })){ this.end_game() }
+			},
+			
+		end_game: function(){
+			$('#cards-wrapper').empty().append('<div id="invitation"><span>Wow, you have a geate memory!<span></div>')
 			}
 	});	
 	
